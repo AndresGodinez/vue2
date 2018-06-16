d1b42a688b57bab38afccded32a603f604b957fb
AGodinez <ing.a.godinez@gmail.com>
2018-06-16 15:50:26 -0500

    gitignore file

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let x = new Vue({
+	el: '#app',
+	data:{
+		name:"Andres",
+		age:28,
+		hobbies:['GOT', 'Ajedres'],
+		live: true,
+		object: {
+			brazos:2,
+			piernas:2,
+			ojos:2,
+			padres:"Mis padres"
+		}
+	}
+});
