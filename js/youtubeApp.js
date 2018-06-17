7d68ba56cc82200b9e249bff18ecd4ef4f92c78a
AGodinez <ing.a.godinez@gmail.com>
2018-06-17 13:36:38 -0500

    only video active have active class

--- js/youtubeApp.js
+++ js/youtubeApp.js
@@ -5,20 +5,23 @@ let v = new Vue({
     title: '',
     activeVideo:'',
     active: '',
-    list: [],
+    listVideos: [],
     showItemInput: false
+  },
+  created(){
+    this.listVideos = JSON.parse(localStorage.getItem('listVideo'));
   },
   methods: {
     addVideoList: function () {
       if (this.urlVideo != '' && this.urlVideo != '') {
-        this.list.push(
+        this.listVideos.push(
           {
             url: this.urlVideo,
             title: this.title,
             active: false
           }
         );
-        localStorage.setItem('listVideo', JSON.stringify(this.list));
+        localStorage.setItem('listVideo', JSON.stringify(this.listVideos));
         this.urlVideo = '';
         this.title = '';
         this.showItemInput = !this.showItemInput;
@@ -33,7 +36,10 @@ let v = new Vue({
       var urlVideo = video.url;
       var newUrl = urlVideo.replace('whatch?v=', 'embed/');
       this.activeVideo = newUrl+'?&autoplay=1';
-
+      for( let i=0; i < this.listVideos.length; i++){
+        this.listVideos[i].active=false;
+      }
+      video.active=true;
     },
     showTitleInput: function () {
       if (this.urlVideo != '')
