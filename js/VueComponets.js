5645105b4ed9c2a84fff0f30098080e5646b1725
AGodinez <ing.a.godinez@gmail.com>
2018-06-17 19:22:53 -0500

    component testing one

---
+++ js/VueComponets.js
@@ -0,0 +1,21 @@
+Vue.component('my-component',
+  {
+    template: '<div>' +
+    '<h1>My component</h1>' +
+    '<span>hello</span>' +
+    '<p>probable</p>' +
+    '<span>{{text}}</span>' +
+    '</div>',
+    data:function (){
+      return {
+        text:'this is a testing text'
+      }
+    },
+  });
+
+new Vue({
+  el: '#app',
+  data: {},
+  methods: {},
+  components: {}
+});
